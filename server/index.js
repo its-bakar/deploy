7782507525836e1c5bd09ada76fd6ae5b94294a0
Bakar <ironsamurai786@gmail.com>
2024-03-13 13:04:44 +0500

    added get request

--- server/index.js
+++ server/index.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const app = express();
 
-app.use("/", (req, res) => {
-  res.send("Server is running!");
+mongoose.connect("mongodb://localhost:27017/crud").then(() => {
+  console.log("Connected to MongoDB!");
+});
+app.get("/", (req, res) => {
+  UserModel.find({})
+    .then((users) => res.json(users))
+    .catch((err) => res.json(err));
 });
 
 app.listen(5000, () => {
